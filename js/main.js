911303363941cab959d98851d13ed4bc8d2a10c1
Michał Łęski <Mleski1997@gmail.com>
2024-03-20 11:32:00 +0100

    added form error and cities

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 const nav = document.querySelector('.navbar-collapse')
+const checkbox = document.getElementById('flexCheckDefault')
+const error = document.querySelector('.form-accept')
+const myForm = document.getElementById('myForm')
+
+document.addEventListener('submit', function(event) {
+    if(!checkbox.checked) {
+        event.preventDefault()
+        error.style.display ='block'
+    }
+})
 
 document.addEventListener('click', () => {
 	if (nav.classList.contains('show')) {
 		nav.classList.remove('show')
 	}
 })
+
+
